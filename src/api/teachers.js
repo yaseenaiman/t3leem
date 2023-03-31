a758da5243c76af6a765e0d0143d24301d9246d1
yaseenaimen <yaseenaiman17@gmail.com>
2023-04-01 01:23:16 +0300

    first commit

---
+++ src/api/teachers.js
@@ -0,0 +1,52 @@
+const teachers =[
+
+  {
+  Id:1,
+  Name:"Hatem", 
+  Position:"Senior Devloper",
+  Location: "Baghdad",
+  Company:"x"
+  
+  },
+  
+  {
+  Id:2,
+  Name:"Ali", 
+  Position:"Senior Accountant",
+  Location: "Baghdad",
+  Company:"x"
+  },
+  {
+  Id:3,
+  Name:"Nina", 
+  Position:"Senior Engineer",
+  Location: "Erbil",
+  Company:"x"
+  
+  },
+  {
+  Id:4,
+  Name:"Julia", 
+  Position:"Secretary",
+  Location: "Baghdad",
+  Company:"x"
+  },
+  {
+  Id:5,
+  Name:"Jack", 
+  Position:"CEO",
+  Location: "Baghdad",
+  Company:"x"
+  
+  }
+  
+  
+  
+  ]
+  
+  export function getUsers(){
+  
+  
+  
+      return teachers;
+  }
